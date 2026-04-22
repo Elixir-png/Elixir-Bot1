d2968f1482b5133dba05bd0885682238897fb6f2
𝑬𝒍𝒊𝒙𝒊𝒓 <165661846+Elixir-png@users.noreply.github.com>
2026-04-22 09:01:31 +0200

    Update elixir.js

--- plugins/elixir.js
+++ plugins/elixir.js
@@ -1,12 +1,12 @@
-let handler = async (m, { conn, command, text }) => {
+let handler = async (m, { conn }) => {
   const message = `ᴇʟɪxɪʀ ᴇ ɪʟ ꜱɪᴄɪʟɪᴀɴᴏ ᴘɪᴜ ꜱɪᴍᴘᴀᴛɪᴄᴏ ᴅᴇʟʟᴇ ᴄᴏᴍᴍ, ɪʟ ᴍɪɢʟɪᴏʀᴇ.
 ɴᴏɴ ʟᴏ ꜰᴀᴛᴇ ɪɴᴄᴀᴢᴢᴀʀᴇ ᴏ ᴠɪ ꜱᴀʟᴛᴀɴᴏ ɪ ɴᴜᴍᴇʀɪ ᴇ ᴘᴀʀᴛᴏɴᴏ ɪ ᴅᴏxx ᴅᴏᴠᴇ ᴠɪ ᴘʀᴇɴᴅᴇ ᴘᴜʀᴇ ɪ ᴘᴇʟɪ ᴅᴇʟ ᴄᴜʟᴏ.`;
-  // manda il messaggio nella chat dove il comando è stato usato, citandolo
+
   await conn.sendMessage(m.chat, { text: message }, { quoted: m });
 };
 
 handler.help = ['elixir'];
 handler.tags = ['giochi'];
-handler.command = /^blood$/i;
+handler.command = /^(blood)$/i; // Corretto il regex per attivarsi con "elixir"
 
 export default handler;
