77a9b051e933fc72db7145e0b07c95d3a1843c9f
𝑬𝒍𝒊𝒙𝒊𝒓 <165661846+Elixir-png@users.noreply.github.com>
2026-04-22 20:05:27 +0200

    Update newsletter name in menu creator

--- plugins/menu-creatore.js
+++ plugins/menu-creatore.js
@@ -77,7 +77,7 @@ let handler = async (m, { conn, usedPrefix: _p, __dirname }) => {
         mentionedJid: [m.sender],
         forwardedNewsletterMessageInfo: {
           newsletterJid: '120363232743845068@newsletter',
-          newsletterName: "✧ ᴇʟɪxɪʀʙᴏᴛ ɢʀᴏᴜᴘ ᴀᴅᴍɪɴ ✧"
+          newsletterName: "✧ ᴇʟɪxɪʀʙᴏᴛ ɢʀᴏᴜᴘ ᴄʀᴇᴀᴛᴏʀᴇ ✧"
         }
       }
     }, { quoted: m })
