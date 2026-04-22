9a1d13fafa6ec5c3e618e35d85cd74a3c159561c
𝑬𝒍𝒊𝒙𝒊𝒓 <165661846+Elixir-png@users.noreply.github.com>
2026-04-22 19:59:30 +0200

    Update menu-ricerche.js

--- plugins/menu-ricerche.js
+++ plugins/menu-ricerche.js
@@ -72,7 +72,7 @@ let handler = async (m, { conn, usedPrefix: _p, __dirname }) => {
         mentionedJid: [m.sender],
         forwardedNewsletterMessageInfo: {
           newsletterJid: '120363232743845068@newsletter',
-          newsletterName: "✧ 𝙱𝙻𝙳-𝙱𝙾𝚃 𝚂𝙴𝙰𝚁𝙲𝙷 𝚂𝚈𝚂𝚃𝙴𝙼 ✧"
+          newsletterName: "✧ ᴇʟɪxɪʀʙᴏᴛ ꜱᴇᴀʀᴄʜ ꜱʏꜱᴛᴇᴍ ✧"
         }
       }
     }, { quoted: m })
