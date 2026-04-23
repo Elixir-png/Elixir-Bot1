9b3362ec115ba4aca2e60ea45e27e6e6c08cd847
𝑬𝒍𝒊𝒙𝒊𝒓 <165661846+Elixir-png@users.noreply.github.com>
2026-04-23 14:35:40 +0200

    Update creatore-unbangp.js

--- plugins/creatore-unbangp.js
+++ plugins/creatore-unbangp.js
@@ -4,7 +4,7 @@ let handler = async (m, { conn, args, isOwner }) => {
         errorMsg += `━━━━━━━━━━━━━━━━\n\n`
         errorMsg += `*⚠️ Motivo:*\n`
         errorMsg += `└─⭓ Comando riservato al proprietario\n\n`
-        errorMsg += `> vare ✧ bot`
+        errorMsg += `> elixir ✧ bot`
         return m.reply(errorMsg)
     }
 
@@ -13,7 +13,7 @@ let handler = async (m, { conn, args, isOwner }) => {
         errorMsg += `━━━━━━━━━━━━━━━━\n\n`
         errorMsg += `*⚠️ Motivo:*\n`
         errorMsg += `└─⭓ Utilizzabile solo nei gruppi\n\n`
-        errorMsg += `> vare ✧ bot`
+        errorMsg += `> elixir ✧ bot`
         return m.reply(errorMsg)
     }
 
@@ -23,7 +23,7 @@ let handler = async (m, { conn, args, isOwner }) => {
         errorMsg += `━━━━━━━━━━━━━━━━\n\n`
         errorMsg += `*⚠️ Motivo:*\n`
         errorMsg += `└─⭓ Questo gruppo non è bannato\n\n`
-        errorMsg += `> vare ✧ bot`
+        errorMsg += `> elixir ✧ bot`
         return m.reply(errorMsg)
     }
 
@@ -36,7 +36,7 @@ let handler = async (m, { conn, args, isOwner }) => {
 *🔓 Azione:* Unban accesso bot
 *📅 Data:* ${new Date().toLocaleString('it-IT')}
 
-> vare ✧ bot`)
+> elixir ✧ bot`)
 }
 
 handler.help = ['unbangp']
